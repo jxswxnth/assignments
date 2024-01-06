60d112b42eb94104e7080e0c0aa38c0271f0cf3d
jxswxnth <jaswanthsai40@gmail.com>
2024-01-06 23:31:53 +0530

    submission middlewares

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,21 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  const userId = req.headers['user-id'];
+
+  if(userId){
+    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] || 0;
+    numberOfRequestsForUser[userId]++;
+
+    if(numberOfRequestsForUser[userId] > 5 ) {
+      res.status(404).send({});
+      return
+    }
+  }
+  next();
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
